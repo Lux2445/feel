f94287de11b00a8f62e6c31410e5f4b0576e7292
Lux2445 <yosinnurov2007@gmail.com>
2024-04-19 22:11:51 +0500

    adaptation sectioln

--- js/app.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const mask = document.querySelector('.mask')
-    window.addEventListener('load', () => {
-        mask.classList.add('hide')
-        setTimeout(() => {
-        mask.remove()
-        }, 2000)
-    })
-})();
