65dd2e5b1a3b9f7d6ff4bdb8191a076f68d331cd
Lux2445 <yosinnurov2007@gmail.com>
2024-04-29 15:31:39 +0500

    load

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 AOS.init();
 
+
+let mask = document.querySelector('.mask')
+window.addEventListener('load', () => {
+    mask.classList.add('.loader_hide')
+    setTimeout(() => {
+        mask.remove()
+    }, 4000)
+});
