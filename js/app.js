f336ed5b6736ec9484ad351f8308466bd9984d29
Lux2445 <yosinnurov2007@gmail.com>
2024-04-30 15:40:45 +0500

    over

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
+
 AOS.init();
 
 
+//срабатывание бургера только в случае полной прогрузки сайта
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function(){
+        document.querySelector(".header").classList.toggle("open")
+    })
+})
+
+// Закрыть меню при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.querySelector(".header").classList.remove("open")
+    }
+});
+
+// Закрыть меню при клике вне его
+document.getElementById("menu").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.body.addEventListener('click', event => {
+    if (event._isClickWithInMenu) return;
+    document.querySelector(".header").classList.remove("open")
+});
+
+//загрузка сайт
 let mask = document.querySelector('.mask')
 window.addEventListener('load', () => {
     mask.classList.add('.loader_hide')
@@ -8,3 +36,4 @@ window.addEventListener('load', () => {
         mask.remove()
     }, 1000)
 });
+
